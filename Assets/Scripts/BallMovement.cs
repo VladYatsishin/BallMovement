823bc460ad9a92a1b5361856f11566f255c13378
Vlad Yatsishin <sunnnnnshinnner445@gmail.com>
2023-06-08 00:36:33 +0300

    removed scripts endings & folder management

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -1,23 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class BallMovementScript : MonoBehaviour
+public class BallMovement : MonoBehaviour
 {
     public float speed;
     Rigidbody rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody>();
+       rb = GetComponent<Rigidbody> ();
     }
 
     void Update()
     {
         float hor = Input.GetAxis("Horizontal");
-        float ver = Input.GetAxis("Vertical");
+        float ver = Input.GetAxis ("Vertical");
         Vector3 movement = new Vector3(hor * speed, rb.velocity.y, ver * speed);
-        rb.AddForce(movement);
+        rb.AddForce (movement);
     }
 }
